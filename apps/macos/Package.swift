fcb78a9c76c24e3a74887bca018adf671033483f
jlwebs <wukangmh@gmail.com>
2026-02-21 11:10:24 +0800

    ci: use xcode 16.2 to support swift 6.2 and Peekaboo main

--- apps/macos/Package.swift
+++ apps/macos/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // Package manifest for the OpenClaw macOS companion (menu bar app + IPC library).
 
 import PackageDescription
@@ -19,7 +19,7 @@ let package = Package(
         .package(url: "https://github.com/swiftlang/swift-subprocess.git", from: "0.1.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.8.0"),
         .package(url: "https://github.com/sparkle-project/Sparkle", from: "2.8.1"),
-        .package(url: "https://github.com/steipete/Peekaboo.git", revision: "04cd768630018a14bce59f90bb276f1c6fb613ac"),
+        .package(url: "https://github.com/steipete/Peekaboo.git", branch: "main"),
         .package(path: "../shared/OpenClawKit"),
         .package(path: "../../Swabble"),
     ],
